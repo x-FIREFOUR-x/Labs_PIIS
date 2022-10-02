5b7c094a72b2e368db801c10da18a922e57402e5
Alex <olexandrpasalsky@gmail.com>
2022-10-02 20:58:54 +0300

    working minimax

--- Labs_PIIS/lab2/lab2/AlphaBetaMiniMax.cpp
+++ Labs_PIIS/lab2/lab2/AlphaBetaMiniMax.cpp
@@ -4,50 +4,54 @@
 
 pair<int, int> AlphaBetaMiniMax::coordinate_move(const Maze& maze, Player player, vector<AbstractEnemy*> enemys)
 {
-    int value = minimax(0, true, MIN_VALUE, MAX_VALUE, maze, player, enemys);
+    pair<int, pair<int, int>> value_move = minimax(0, true, MIN_VALUE, MAX_VALUE, maze, player, enemys);
 
-    return pair<int, int>();
+    return value_move.second;
 }
 
-int AlphaBetaMiniMax::minimax(int depth, bool is_maximizing, int alpha, int beta, const Maze& maze, Player player, vector<AbstractEnemy*> enemys)
+pair<int, pair<int, int>> AlphaBetaMiniMax::minimax(int depth, bool is_maximizing, int alpha, int beta, const Maze& maze, Player player, vector<AbstractEnemy*> enemys)
 {
     if (depth == MAX_DEPTH || is_terminal(maze, player, enemys))
-        return calculate_value(maze, player, enemys);
+        return { calculate_value(maze, player, enemys), player.get_coordinates() };
 
     if (is_maximizing)
     {
         int best = MIN_VALUE;
+        pair<int, int> best_move;
 
         vector<Player> states_player = get_states_player(maze, player);
         for (int i = 0; i < states_player.size(); i++)
         {
-            int val = minimax(depth + 1, false, alpha, beta, maze, states_player[i], enemys);
+            pair<int, pair<int, int>> val_move = minimax(depth + 1, false, alpha, beta, maze, states_player[i], enemys);
 
-            best = max(best, val);
+            if (val_move.first > best)
+            {
+                best = val_move.first;
+                best_move = val_move.second;
+            }
 
             alpha = max(alpha, best);
             if (beta <= alpha)
                 break;
         }
-        return best;
+        return { best, best_move };
     }
     else
     {
         int best = MAX_VALUE;
-        pair<int, int> cur_move_player;
+        pair<int, int> best_move = player.get_coordinates();
 
         vector<vector<AbstractEnemy*>> state_enemys = get_states_enemys(maze, enemys);
         for (int i = 0; i < state_enemys.size(); i++)
         {
-            int val = minimax(depth + 1, true, alpha, beta, maze, player, state_enemys[i]);
-            best = min(best, val);
-
+            pair<int, pair<int, int>> val_move = minimax(depth + 1, true, alpha, beta, maze, player, state_enemys[i]);
+            best = min(best, val_move.first);
 
             beta = min(beta, best);
             if (beta <= alpha)
                 break;
         }
-        return best;
+        return { best, best_move };
     }
 }
 
--- Labs_PIIS/lab2/lab2/AlphaBetaMiniMax.h
+++ Labs_PIIS/lab2/lab2/AlphaBetaMiniMax.h
@@ -15,7 +15,7 @@ public:
 	pair<int, int> coordinate_move(const Maze& maze, Player player, vector<AbstractEnemy*> enemys) override;
 
 private:
-	int minimax(int depth, bool is_maximizing, int alpha, int beta, const Maze& maze, Player player, vector<AbstractEnemy*> enemys);
+	pair<int, pair<int,int>> minimax(int depth, bool is_maximizing, int alpha, int beta, const Maze& maze, Player player, vector<AbstractEnemy*> enemys);
 
 	int calculate_value(const Maze& maze, const Player& player, const vector<AbstractEnemy*>& enemys);
 	bool is_terminal(const Maze& maze, const Player& player, const vector<AbstractEnemy*>& enemys);
